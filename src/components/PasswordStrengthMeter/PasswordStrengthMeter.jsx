2755511afe14c8fc160b8f337f7c245b019f7af0
Santiago Cordero <santicordero28@gmail.com>
2026-05-17 01:02:02 -0600

    Botón para mostrar/esconder contraseña

--- src/components/PasswordStrengthMeter/PasswordStrengthMeter.jsx
+++ src/components/PasswordStrengthMeter/PasswordStrengthMeter.jsx
@@ -4,6 +4,7 @@ import evalPassword from './evalPassword.jsx'
 
 const PasswordStrengthMeter = () => {
     const [password, setPassword] = useState('')
+    const [showPass, setShowPass] = useState(false)
     const strength = evalPassword(password)
     const strengthClass = strength.replaceAll(' ', '-')
 
@@ -14,13 +15,18 @@ const PasswordStrengthMeter = () => {
 
     return (<div className='password-meter'>
         <p className="password-meter-guide">Ingresa tu contraseña</p>
-        <input
-            className='password-meter-input'
-            type='password'
-            placeholder='contraseña123...'
-            value={password}
-            onChange={(e) => { setPassword(e.target.value) }}
-        />
+        <div className="password-meter-input-div">
+            <input
+                className='password-meter-input'
+                type={showPass ? 'text' : 'password'}
+                placeholder='contraseña123...'
+                value={password}
+                onChange={(e) => { setPassword(e.target.value) }}
+            />
+            <button type="button" className="password-meter-visiblebtn" onClick={() => { setShowPass(!showPass) }}>
+                {showPass ? '👁️‍🗨️' : '🫣'}
+            </button>
+        </div>
         <p className={`password-meter-label password-${strengthClass}`}>Fortaleza de Contraseña: {strength}</p>
 
     </div>)
